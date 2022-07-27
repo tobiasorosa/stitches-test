0ceca1551f9ff6663bf38739f5f7cec0e912e8fe
Tobias <tobias.oliveira@idinheiro.com.br>
2022-07-27 16:45:39 -0300

    Added global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,51 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { globalCss } from '@stitches/react';
+
+const globalStyles = globalCss({
+	'*': {
+		margin: 0,
+		padding: 0,
+		boxSizing: 'border-box',
+	},
+
+	html: {
+		fontFamily: 'Overpass, sans-serif',
+	},
+	body: {
+		fontFamily: 'Overpass, Arial, sans-serif',
+		fontWeight: 400,
+	},
+	a: {
+		textDecoration: 'none',
+		backgroundColor: 'transparent',
+	},
+	p: {
+		fontSize: '16px',
+		marginTop: 0,
+		marginBottom: '1rem',
+	},
+	'img, svg': {
+		maxWidth: '100%',
+		height: 'auto',
+	},
+	'ul, ol': {
+		listStylePosition: 'inside',
+	},
+	li: {
+		marginBottom: '0.625rem',
+	},
+	fieldset: {
+		minWidth: 0,
+		padding: 0,
+		margin: 0,
+		border: 0,
+	},
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	globalStyles();
+
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
